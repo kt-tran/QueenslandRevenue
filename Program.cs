01a4e6eb3e9156adffd9af1a822bc18e0e4a66b0
p4paya <katherinetran16@gmail.com>
2022-08-31 18:22:28 +1000

    tweak Task 4 & add to Task 5

--- Program.cs
+++ Program.cs
@@ -35,7 +35,6 @@ namespace QueenslandRevenue
 
 
             //lastYear code is repeated for this year input
-
             Console.Write("Please enter the number of contestants who entered in this year's competition: ");
             while (true)
             {
@@ -51,34 +50,52 @@ namespace QueenslandRevenue
                 }
             }
 
+
             //Task 3: Display input data & revenue for this year's competition
             int revenue;
-            revenue = thisYear * 25;
-            Console.WriteLine("{0} contestants entered last year's competition." +
-                "The revenue for this year's competition is ${1} from the {2} contestants who entered.",
-                lastYear, revenue, thisYear);
+            
+            revenue = thisYear * 25; //total revenue = (number of contestants x $25 per contestant)
+            
+            //print to user
+            Console.WriteLine("{0} contestants entered last year's competition. " +
+                "{1} contestants entered this year's competition. " +
+                "The revenue for this year's competition is ${2}.",
+                lastYear, thisYear, revenue);
 
-            /*
+
             //Task 4: Display statement comparing number of contestants for this year vs last year
             int doubleLastYear = lastYear * 2;
-            if (thisYear >= doubleLastYear)
-                Console.WriteLine("The competition is more than twice as big this year!");
-            else if (thisYear > lastYear)
+
+            if (thisYear > lastYear)
                 Console.WriteLine("The competition is bigger than ever!");
+            else if (thisYear >= doubleLastYear)
+                Console.WriteLine("The competition is more than twice as big this year!");
             else if (thisYear <= lastYear)
                 Console.WriteLine("A tighter race this year! Come out and cast your vote!");
 
-            //Task 5: Contestant talents
-            Console.WriteLine("The talents and names of the contestants are required.");
+            //TODO
+            //Task 5: Contestant talents data entry & search
+            string[] names = new string[thisYear];
+            char[] talents = new char[thisYear];
+            bool validTalent = false;
+            Console.WriteLine("The talents and names of the contestants for this year are required.");
 
-            while //TODO
-            Console.Write("Please enter the name of the contestant: ");
-            Console.Write("Please enter the contestant's talent: S for singing, D for dancing, " +
-                "M for playing a musical instrument, or O for other: ");
-            //if !(USER INPUT SHOULD = S, D, M OR O)
-            Console.WriteLine("That was not a valid code. Please enter S for singing, D for dancing," +
+            for (int i = 0; i < thisYear; i++)
+            {
+                Console.Write("Please enter the name of the contestant: ");
+                names[i] = Console.ReadLine();
+                Console.Write("Please enter {0}'s special talent: S for singing, D for dancing, " +
+                "M for playing a musical instrument, or O for other: ", names[i]);
+                talents[i] = Char.Parse(Console.ReadLine());
+                validTalent = (talents[i] == 'S' || talents[i] == 'D' || talents[i] == 'M' || talents[i] == 'O');
+                while (validTalent)
+                {
+                    Console.WriteLine("That was not a valid code. Please enter S for singing, D for dancing," +
                 "M for playing a musical instrument or 0 for other: ");
-            */
+
+                }
+            }
+            
         }
     }
 }
