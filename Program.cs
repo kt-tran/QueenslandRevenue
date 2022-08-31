5d90cb938423674d83afc146b28cd35cb1020d62
p4paya <katherinetran16@gmail.com>
2022-08-31 17:51:56 +1000

    while loop for task 2 + grammar mistakes

--- Program.cs
+++ Program.cs
@@ -12,31 +12,53 @@ namespace QueenslandRevenue
             Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++");
 
             //Task 2: Prompt user for number of contestants
-            int lastYear, thisYear;
+            int lastYear = 0;
+            int thisYear;
             const int LOWER = 0, UPPER = 30;
 
-            //while 
-            Console.Write("Please enter the number of contestants who entered in last year's competition: ");
-            lastYear = Convert.ToInt32(Console.ReadLine());
-            if (lastYear >= LOWER && lastYear <= UPPER)
+            Console.Write("Please enter the number of contestants who entered in last year's competition, " +
+                "within the range of {0} and {1}: ", LOWER, UPPER);
+
+            while (true)
             {
-                Console.WriteLine("Confirmed. {0} contestants entered in last year's competition.",
-                    lastYear);
+                lastYear = Convert.ToInt32(Console.ReadLine());
+                if (lastYear >= LOWER && lastYear <= UPPER)
+                {
+                    Console.WriteLine("Confirmed. {0} contestants entered in last year's competition.", lastYear);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid entry. Please enter a number between 0 and 30.");
+                }
             }
-            else
-            {
-                Console.WriteLine("Invalid entry. Please enter a number between 0 and 30.");
-            }
+
+
+            //lastYear code is repeated for this year input
+
             Console.Write("Please enter the number of contestants who entered in this year's competition: ");
-            thisYear = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                thisYear = Convert.ToInt32(Console.ReadLine());
+                if (thisYear >= LOWER && thisYear <= UPPER)
+                {
+                    Console.WriteLine("Confirmed. {0} contestants entered this year.", thisYear);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid entry. Please enter a number between 0 and 30.");
+                }
+            }
 
             //Task 3: Display input data & revenue for this year's competition
             int revenue;
             revenue = thisYear * 25;
             Console.WriteLine("{0} contestants entered last year's competition." +
-                "The revenue for this year's competition is {1} for the {2} contestants who entered.",
+                "The revenue for this year's competition is ${1} from the {2} contestants who entered.",
                 lastYear, revenue, thisYear);
 
+            /*
             //Task 4: Display statement comparing number of contestants for this year vs last year
             int doubleLastYear = lastYear * 2;
             if (thisYear >= doubleLastYear)
@@ -53,6 +75,10 @@ namespace QueenslandRevenue
             Console.Write("Please enter the name of the contestant: ");
             Console.Write("Please enter the contestant's talent: S for singing, D for dancing, " +
                 "M for playing a musical instrument, or O for other: ");
+            //if !(USER INPUT SHOULD = S, D, M OR O)
+            Console.WriteLine("That was not a valid code. Please enter S for singing, D for dancing," +
+                "M for playing a musical instrument or 0 for other: ");
+            */
         }
     }
 }
